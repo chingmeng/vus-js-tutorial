1d8d86f1c1f8a6ca7c1de13f21e1c3ce9288b1f2
chingmeng <sum2infinity@gmail.com>
2020-11-08 12:39:06 +0800

    Add Todos

---
+++ src/components/Todos.vue
@@ -0,0 +1,54 @@
+<template>
+
+<div>
+  <AddTodo v-on:add-todo="addTodo"/>
+  <div v-bind:key="todo.id" v-for="todo in todos">
+    <TodoItem v-bind:todo="todo" 
+        v-bind:markComplete="markComplete"
+        v-on:del-todo="deleteTodo"
+    />
+
+  </div>
+</div>
+
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+import AddTodo from './AddTodo.vue';
+
+export default {
+  name: "Todos",
+  components: {
+    TodoItem,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "Todo One", completed: true },
+        { id: 2, title: "Todo Two", completed: true },
+        { id: 3, title: "Todo Three", completed: false }
+      ] 
+    }
+  },
+  methods: {
+    markComplete(id) {
+      this.todos.forEach(e => console.log(e));
+      const todo = this.todos.filter(e => e.id === id )[0];
+      todo.completed = !todo.completed; 
+    },
+    deleteTodo(id) {
+      this.todos = this.todos.filter(e => e.id !== id);
+    },
+    addTodo(newTodo) {
+      console.log(newTodo);
+      this.todos = [...this.todos, newTodo];
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
