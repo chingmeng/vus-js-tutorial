8f822856ae16f98ae377e027d7c4f5adace08c90
chingmeng <sum2infinity@gmail.com>
2020-11-08 19:04:47 +0800

    Setup path for TodoForm

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/todo_form/:id/:title/:detail/:completed',
+    name: 'Todo Form',
+    component: () => import('../components/TodoForm.vue')
+  },
+  {
+    path: '/todo_form/',
+    name: 'Todo Form Create',
+    component: () => import('../components/TodoForm.vue')
   }
 ]
 
