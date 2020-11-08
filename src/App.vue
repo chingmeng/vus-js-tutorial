7afb8d10d08620112bb187d3df2a665ef701254e
chingmeng <sum2infinity@gmail.com>
2020-11-08 13:19:11 +0800

    Add vue-router via vue ui

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,30 @@
 <template>
-  <div id="app">
-    <Header />
-    <Todos />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
 
-<script>
-import Header from './components/layout/Header';
-import Todos from './components/Todos';
-
-export default {
-  name: 'App',
-  components: {
-    Todos,
-    Header
-  }
-}
-</script>
-
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
+#nav {
+  padding: 30px;
 }
 
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
-  color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-.btn:hover {
-  background: #666;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
-
 </style>
